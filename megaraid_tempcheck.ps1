c4b1b85eb36618651fd830671d6ad66451070042
mweissboston <111768361+mweissboston@users.noreply.github.com>
2023-08-31 10:41:31 +0200

    Create megaraid_tempcheck.ps1

---
+++ megaraid_tempcheck.ps1
@@ -0,0 +1,52 @@
+$storcli = "C:\tools\storcli64.exe"
+$commandOutput = & $storcli " /call/eall/sall show all"
+
+$drivePattern = "Drive /c(\d+)/e(\d{1,3})/s(\d+) - Detailed Information"
+$temperaturePattern = "Drive Temperature =\s+(\d+)C"
+$snPattern = "SN =\s+([A-Za-z0-9]+)"
+
+$driveInfo = @()
+
+$controllerNumber = ""
+$enclosureNumber = ""
+$slotNumber = ""
+$temperatureValue = ""
+$snValue = ""
+
+foreach ($line in $commandOutput -split "`n") {
+    if ($line -match $drivePattern) {
+        $controllerNumber = $Matches[1]
+        $enclosureNumber = ("{0:D3}" -f [int]$Matches[2])
+        $slotNumber = $Matches[3]
+        $slotNumber = if ($slotNumber -match '^\d$') { "0$slotNumber" } else { $slotNumber }
+    } elseif ($line -match $temperaturePattern) {
+        $temperatureValue = $Matches[1]
+    } elseif ($line -match $snPattern) {
+        $snValue = $Matches[1]
+        
+        $driveInfo += @{
+            Controller = $controllerNumber
+            Enclosure = $enclosureNumber
+            Slot = $slotNumber
+            Temperature = $temperatureValue
+            SN = $snValue
+        }
+
+        $controllerNumber = ""
+        $enclosureNumber = ""
+        $slotNumber = ""
+        $temperatureValue = ""
+        $snValue = ""
+    }
+}
+
+$output = @"
+<<<local:sep(0)>>>`n
+"@
+foreach ($drive in $driveInfo) {
+    $output += @"
+P "MR C$($drive.Controller) - Enclosure $($drive.Enclosure) - Slot $($drive.Slot)" temperature=$($drive.Temperature);45;50;0;100 SN: $($drive.SN)`n
+"@
+}
+
+Write-Output $output
